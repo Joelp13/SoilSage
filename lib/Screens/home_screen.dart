90dd15ce9e34ffd0c0b49998c49e5277b088be62
Joel Martin <joelmartinp13@gmail.com>
2023-11-08 23:16:47 +0530

    Added a row

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -5,6 +5,38 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xFFeeedf2),
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                    height: 30,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/images/user.png")),
+                    )),
+                Container(
+                    height: 30,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/images/down.png")),
+                    ))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
