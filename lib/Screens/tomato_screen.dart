29c3f418ae6cad5f223da65b44a270f57343e6e0
Joel Martin <joelmartinp13@gmail.com>
2023-11-10 23:46:04 +0530

    Tomato

--- lib/Screens/tomato_screen.dart
+++ lib/Screens/tomato_screen.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:soil_sage/Screens/home_screen.dart';
 
 class TomatoScreen extends StatelessWidget {
   const TomatoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 174, 238, 204),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.white,
+                child: IconButton(
+                  color: Color(0xff4c505b),
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                        context as BuildContext,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                const HomeScreen()));
+                  },
+                  icon: Icon(Icons.arrow_back),
+                  iconSize: 20,
+                ),
+              ),
+              CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.white,
+                child: IconButton(
+                  color: Color(0xff4c505b),
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                        context as BuildContext,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                const HomeScreen()));
+                  },
+                  icon: Icon(Icons.heart_broken_rounded),
+                  iconSize: 20,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
