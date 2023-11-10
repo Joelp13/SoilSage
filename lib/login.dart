94477155028c4c5864f0026b05873d521660aff4
abhiramvijay <abhiramvijaykumar15@gmail.com>
2023-11-10 19:23:26 +0530

    loginpage completed

--- lib/login.dart
+++ lib/login.dart
@@ -15,7 +15,7 @@ class _MyloginState extends State<Mylogin> {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/images/theme.jpg'),fit: BoxFit.cover
+          image: AssetImage('assets/images/screen2.jpg'),fit: BoxFit.cover
         )
       ),
       child: Scaffold(
@@ -23,24 +23,25 @@ class _MyloginState extends State<Mylogin> {
         body: Stack(
           children: [
             Container(
-              padding: EdgeInsets.only(left: 20,top: 80),
+              padding: EdgeInsets.only(left: 20,top: 200),
               child: Text('Welcome\nBack!',style: TextStyle(
-                color: Colors.black,
+                color: Colors.white70,
                 fontSize: 33,
                 fontWeight: FontWeight.bold,
               ),),
             ),
             SingleChildScrollView(
               child: Container(
-                padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.3,right: 35,left: 35),
+                padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.4,right: 35,left: 35),
               child: Column(
                 children: [
                   TextField(
-                    obscureText: true,
                     decoration: InputDecoration(
+                      prefixIcon:Icon(Icons.person),
                       fillColor: Colors.grey.shade100,
                       filled: true,
                       hintText: 'Email',
+                       hintStyle:TextStyle(fontWeight: FontWeight.bold ,color: Colors.teal.shade900),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10)
                       )
@@ -50,11 +51,12 @@ class _MyloginState extends State<Mylogin> {
                     height: 30,
                   ),
                   TextField(
-                    obscureText: true,
                     decoration: InputDecoration(
+                      prefixIcon:Icon(Icons.lock),
                       fillColor: Colors.grey.shade100,
                       filled: true,
                       hintText: 'Password',
+                       hintStyle:TextStyle(fontWeight: FontWeight.bold ,color: Colors.teal.shade900),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10)
                       )
@@ -63,24 +65,26 @@ class _MyloginState extends State<Mylogin> {
                   SizedBox(
                     height: 30,
                   ),
-                  TextField(
+                  /*TextField(
                     obscureText: true,
                     decoration: InputDecoration(
+                      prefixIcon:Icon(Icons.location_city),
                       fillColor: Colors.grey.shade100,
                       filled: true,
                       hintText: 'Panchayat',
+                      hintStyle:TextStyle(fontWeight: FontWeight.bold ,color: Colors.teal.shade900),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10)
                       )
                     ),
-                  ),
+                  ),*/
                   SizedBox(
                     height: 30,
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Text('Sign In',style: TextStyle(color: Color(0xff4c505b),
+                      Text('Sign In',style: TextStyle(color: Colors.white,
                         fontSize: 27,fontWeight: FontWeight.w700
                       ),),
                       SizedBox(
